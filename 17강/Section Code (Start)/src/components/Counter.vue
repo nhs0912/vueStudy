3f22e738679136c12d66f7498f4368505572cb0b
HeeSeok Noh <nhs0912@nate.com>
2020-12-05 23:59:03 +0900

    15,16,17강 등록

---
+++ 17강/Section Code (Start)/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increment() {
+                this.$store.state.counter++;
+                //this.$emit('updated', 1);
+            },
+            decrement() {
+                this.$store.state.counter--;
+                // this.$emit('updated', -1);
+            }
+        }
+    }
+</script>
---
+++ 17강/Section Code (Start)/src/components/Result.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>Counter is: {{ counter }}</p>
+</template>
+
+<script>
+    export default {
+        computed:{
+            counter(){
+                return this.$store.state.counter * 2;
+            }
+
+        }
+        //  props: ['counter']
+    }
+</script>
