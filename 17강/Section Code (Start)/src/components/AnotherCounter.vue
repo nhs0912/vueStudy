78ee80e35fa588e0a8b403cc19d51105bffbcd6e
HeeSeok Noh <nhs0912@nate.com>
2020-12-13 19:11:22 +0900

    18강 기본setting 완료

---
+++ 17강/Section Code (Start)/src/components/AnotherCounter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- <p>Counter is : {{ counter }}</p> -->
+        <p>Counter is : {{ doubleCounter }}</p>
+        <!-- <p> Number of Clicks : {{clicks}}</p> -->
+        <p> Number of Clicks : {{stringCounter}}</p>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    export default {
+
+        coumputed : mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ])
+    // computed:{
+    //     counter(){
+    //         return this.$store.getters.doubleCounter;
+    //         // return this.$store.state.counter * 2;
+    //     },
+    //     clicks(){
+    //         return this.$store.getters.stringCounter;
+    //     }
+    // }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 18강/Section Code (Start)/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
+
+
+
+
